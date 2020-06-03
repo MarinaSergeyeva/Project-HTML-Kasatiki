55a838531ccafe27bfb4b2418964b20ba1f6a3af
Marina Sergeyeva <morrrinad@gmail.com>
2020-06-03 20:43:56 +0300

    header started

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const { series, parallel, watch } = require('gulp');
+const {
+  series,
+  parallel,
+  watch
+} = require('gulp');
 const requireDir = require('require-dir');
 const browserSync = require('browser-sync').create();
 
-const tasks = requireDir('./gulp/tasks', { recurse: true });
+const tasks = requireDir('./gulp/tasks', {
+  recurse: true
+});
 const paths = require('./gulp/paths');
 
 const serve = () => {
@@ -25,7 +31,9 @@ const watcher = done => {
     'change',
     series(tasks.html, tasks.inject, browserSync.reload),
   );
-  watch(paths.watch.css).on('change', series(tasks.css, browserSync.reload));
+  watch(paths.watch.css).on('change', function () {
+    setTimeout(series(tasks.css, browserSync.reload), 300)
+  });
   watch(paths.watch.js).on('change', series(tasks.scripts, browserSync.reload));
   watch(paths.watch.images, tasks.images);
   watch(paths.watch.fonts, tasks.fonts);
