47f4a53252023de49fa1d33ea1145ea9c47612c0
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2020-06-04 14:38:49 +0300

    second

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,27 +1,41 @@
-$(document).ready(function () {
-  $('.teachers-list').slick({
-    centerMode: true,
-    infinite: true,
-    centerPadding: '60px',
+$('.teachers-list').slick({
+    arrows: true,
     slidesToShow: 3,
-    responsive: [{
+    centerPadding: '60px',
+    infinite: false,
+    nextArrow: '<button type="button" class="slick-next">&#8594;</button>',
+    prevArrow: '<button type="button" class="slick-prev">&#8592;</button>',
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          infinite: false,
+          slidesToShow: 1
+         
+        }
+      },
+      {
         breakpoint: 768,
         settings: {
           arrows: true,
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 2
+          infinite: false,
+          slidesToShow: 3
+         
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 500,
         settings: {
           arrows: true,
-          centerMode: true,
+          infinite: false,
           centerPadding: '40px',
           slidesToShow: 1
         }
       }
     ]
   });
-});
