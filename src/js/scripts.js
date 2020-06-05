7676e25162802186f5ab551b8cb5bd27d06a5180
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2020-06-05 15:01:53 +0300

    commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,34 +1,33 @@
-$('.teachers-list').slick({
-  arrows: true,
-  slidesToShow: 3,
-  centerPadding: '20px',
-  infinite: true,
-  nextArrow: '<button type="button" class="slick-next">&#8594;</button>',
-  prevArrow: '<button type="button" class="slick-prev">&#8592;</button>',
-  responsive: [{
-      breakpoint: 1439,
-      settings: {
-        variablesWidth: false,
-        arrows: true,
-        centerMode: true,
-        centerPadding: '25%',
-        infinite: true,
-        slidesToShow: 1
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        arrows: true,
-        infinite: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-})
-
-
-// .on('setPosition', function (event, slick) {
-//   slick.$slides.css('height', slick.$slideTrack.height() + 'px');
-//  });
+$(document).ready(function () {
+  $('.teachers-list').slick({
+    arrows: true,
+    slidesToShow: 3,
+    centerPadding: '20px',
+    nextArrow: '<button type="button" class="slick-next">&#8594;</button>',
+    prevArrow: '<button type="button" class="slick-prev">&#8592;</button>',
+    responsive: [{
+        breakpoint: 1439,
+        settings: {
+          variablesWidth: false,
+          arrows: true,
+          centerMode: true,
+          centerPadding: '25%',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $('.teachers-list').on('setPosition', function (event, slick) {
+    $(slick.$slides)
+      .height('auto')
+      .css('height', $(slick.$slideTrack).height() + 'px');
+  });
+});
