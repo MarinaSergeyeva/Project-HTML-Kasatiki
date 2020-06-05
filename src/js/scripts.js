036e8db1b7e828e897512dafd02fafacabe6769d
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2020-06-05 10:05:31 +0300

    added hover

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,35 +1,38 @@
 $('.teachers-list').slick({
-    arrows: true,
-    slidesToShow: 3,
-    centerPadding: '20px',
-    infinite: true,
-    nextArrow: '<button type="button" class="slick-next">&#8594;</button>',
-    prevArrow: '<button type="button" class="slick-prev">&#8592;</button>',
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
+  arrows: true,
+  slidesToShow: 3,
+  centerPadding: '20px',
+  infinite: true,
+  nextArrow: '<button type="button" class="slick-next">&#8594;</button>',
+  prevArrow: '<button type="button" class="slick-prev">&#8592;</button>',
+  responsive: [{
+      breakpoint: 1439,
+      settings: {
         variablesWidth: false,
-          arrows: true,
-          centerMode: true,
-          centerPadding: '25%',
-          infinite: true,
-          slidesToShow: 1
-         
-        }
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          arrows: true,
-          infinite: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
+        arrows: true,
+        centerMode: true,
+        centerPadding: '25%',
+        infinite: true,
+        slidesToShow: 1
       }
-    ]
-  })
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: true,
+        infinite: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+})
 
-  .on('setPosition', function (event, slick) {
-    slick.$slides.css('height', slick.$slideTrack.height() + 'px');
-  });
+//  .on('setPosition', function (event, slick) {
+//    slick.$slideTrack.children().css('height', slick.$slideTrack.height() + 'px');
+// });
+
+
+// .on('setPosition', function (event, slick) {
+//  slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+// });
